b6e2dde53950ad08d832a3dfd96f779e5bfcca45
MollyK-A <126957884+jessicajacobson@users.noreply.github.com>
2023-05-14 19:44:26 +0200

    dialogue for scene1_1, 1_2 and 1_3

---
+++ assets/dialogue_script/scene1_3.ts
@@ -0,0 +1,10 @@
+const scene1_3: Line[] = [
+    molly('Wow, vilka bra låtar! Det måste vara en helt otrolig spelman.'),
+    oskar('Eller hur! Det sägs att han sitter på Stora Fjärran Berget och att den som går upp dit får lära sig världens bästa låt. '),
+    molly('Det låter som nåt för dig, Adam! Du är ju världens frilufsmänniska. Vad säger du, vågar du ta dig an den största spelmansutmaningen av dem alla?', [
+        'Okej', 'Njaaa'
+    ]),
+    oskar('Nice! Jag har annat för mig så du får göra det själv.'),
+    molly('Öh, jag med. Men love that journey for you!'),
+    adam('Haha, okej.'),
+];
