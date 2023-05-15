cacb88b67afa96f7519fff6a2e0bae9d1ad1f283
oskar olausson <oskar@oskars-MacBook-Pro.local>
2023-05-15 08:38:26 +0200

    better keyboard logic

--- src/scenes/battle-scene.ts
+++ src/scenes/battle-scene.ts
@@ -1,20 +1,22 @@
-import 'phaser';
-import gaspUrl from '../../assets/gasp.mp3';
-import lineUrl from '../../assets/line.png';
-import skaningenUrl from '../../assets/skaningen.mp3';
-import playerUrl from '../../assets/adam.png';
-import sheetUrl from '../../assets/sheet.png';
-import backgroundUrl from '../../assets/background.png';
-import noteUrl from '../../assets/note.png';
-import enemyUrl from '../../assets/uffe.png';
-import { displayEnemyStats, Enemy } from '../enemy';
-import { displayPlayerStats, Player } from '../player';
-import { isAllowed, playNote, skaningen, Song } from '../songs';
-import { createSheet, Sheet } from '../sheet';
+import "phaser";
+import gaspUrl from "../../assets/gasp.mp3";
+import lineUrl from "../../assets/line.png";
+import skaningenUrl from "../../assets/skaningen.mp3";
+import playerUrl from "../../assets/adam.png";
+import sheetUrl from "../../assets/sheet.png";
+import backgroundUrl from "../../assets/background.png";
+import noteUrl from "../../assets/note.png";
+import enemyUrl from "../../assets/uffe.png";
+import { displayEnemyStats, Enemy } from "../enemy";
+import { displayPlayerStats, Player } from "../player";
+import { isAllowed, playNote, skaningen, Song } from "../songs";
+import { createSheet, Sheet } from "../sheet";
 
-export const battleSceneKey = 'BattleScene';
+export const battleSceneKey = "BattleScene";
 
-export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scenes.CreateSceneFromObjectConfig {
+export function battle():
+  | Phaser.Types.Scenes.SettingsConfig
+  | Phaser.Types.Scenes.CreateSceneFromObjectConfig {
   let keys: {
     S: Phaser.Input.Keyboard.Key;
     G: Phaser.Input.Keyboard.Key;
@@ -28,37 +30,48 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
   let sheet: Sheet;
   let line: {
     s: Phaser.GameObjects.Image;
-    t: number,
+    t: number;
   };
   let song: undefined | Song;
-  let playedNotes: {s: Phaser.GameObjects.Image, hit: boolean}[] = [];
+  let playedNotes: { s: Phaser.GameObjects.Image; hit: boolean }[] = [];
 
   let yourTurn = true;
 
   return {
     key: battleSceneKey,
     preload() {
-      if (this.input.keyboard) {
-        keys = {
-          S: this.input.keyboard.addKey(
-            Phaser.Input.Keyboard.KeyCodes.S,
-          ),
-          G: this.input.keyboard.addKey(
-            Phaser.Input.Keyboard.KeyCodes.G,
-          ),
-          D: this.input.keyboard.addKey(
-            Phaser.Input.Keyboard.KeyCodes.D,
-          ),
-          A: this.input.keyboard.addKey(
-            Phaser.Input.Keyboard.KeyCodes.A,
-          ),
-          E: this.input.keyboard.addKey(
-            Phaser.Input.Keyboard.KeyCodes.E,
-          ),
-        }
-      } else {
-        throw Error('no keyboard, what');
+      if (!this.input.keyboard) {
+        throw Error("no keyboard, what");
       }
+
+      keys = {
+        S: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+        G: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.G),
+        D: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+        A: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+        E: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
+      };
+
+      this.input.keyboard.on("keydown", (ev: { key: string }) => {
+        const el = document.getElementById("keypresses") as HTMLElement;
+        if (ev.key.toUpperCase() === "Q") {
+          el.innerHTML += "," + line.t;
+        } else if (ev.key.toUpperCase() === "W") {
+          el.innerHTML = "";
+        }
+
+        if (!isAllowed(ev.key)) {
+          return;
+        }
+
+        if (!song) {
+          return;
+        }
+
+        const { x, y, hit } = playNote(line.t, ev.key, song, sheet);
+        const note = { s: this.add.image(x, y, "note"), hit };
+        playedNotes.push(note);
+      });
 
       keys.S.isDown = false;
       keys.G.isDown = false;
@@ -77,14 +90,12 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
       this.load.image(lineUrl, lineUrl);
       this.load.image(backgroundUrl, backgroundUrl);
       // so that we can easily refer to it in other files easily (level file)
-      this.load.image('note', noteUrl);
-      this.load.image('sheet', sheetUrl);
-
+      this.load.image("note", noteUrl);
+      this.load.image("sheet", sheetUrl);
     },
     create() {
       this.add.image(0, 0, backgroundUrl).setOrigin(0, 0);
       sheet = createSheet(this);
-
 
       line = {
         s: this.add.image(300, 20, lineUrl),
@@ -95,43 +106,17 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
 
       player = {
         s: this.add.image(100, 300, playerUrl),
-        text: this.add.text(120, 320, '', {
-          fontSize: '20px',
+        text: this.add.text(120, 320, "", {
+          fontSize: "20px",
           fontFamily: "Helvetica",
         }),
         hp: 40,
-      }
-
-      const scene = this;
-
-      document.addEventListener('keydown', (ev) => {
-        const el = document.getElementById('keypresses') as HTMLElement;
-
-        if (!song) {
-          return;
-        }
-
-        if (isAllowed(ev.key)) {
-          const {x, y, hit} = playNote(line.t, ev.key, song, sheet);
-
-          const note = { s: scene.add.image(x, y, 'note'), hit};
-
-          playedNotes.push(
-            note
-          );
-        }
-
-        if (ev.key.toUpperCase() === 'Q') {
-          el.innerHTML += ',' + line.t;
-        } else if (ev.key.toUpperCase() === 'W') {
-          el.innerHTML = '';
-        }
-      })
+      };
 
       enemy = {
         s: this.add.image(650, 300, enemyUrl),
-        text: this.add.text(650, 10, '', {
-          fontSize: '20px',
+        text: this.add.text(650, 10, "", {
+          fontSize: "20px",
           fontFamily: "Helvetica",
         }),
         confused: false,
@@ -139,7 +124,7 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
         resistGroove: 30,
         resistSleep: 40,
         hasEarMuffs: false,
-      }
+      };
 
       displayEnemyStats(enemy);
       displayPlayerStats(player);
@@ -148,21 +133,25 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
       if (song) {
         line.t += 1;
 
-        line.s.x = sheet.innerX() + sheet.innerWidth() * ((line.t - song.startsAt) / (song.endsAt - song.startsAt));
+        line.s.x =
+          sheet.innerX() +
+          sheet.innerWidth() *
+            ((line.t - song.startsAt) / (song.endsAt - song.startsAt));
 
         if (line.t > song.endsAt) {
           line.s.setVisible(false);
         }
+
         if (line.t > song.fullEnd) {
           yourTurn = false;
 
-          if (song.name === 'skaningen') {
+          if (song.name === "skaningen") {
             enemy.resistFear -= 1;
             displayEnemyStats(enemy);
           }
 
           const score = clearNotes(playedNotes);
-          clearSong(song)
+          clearSong(song);
           song = undefined;
 
           enemy.resistFear -= 1 + score;
@@ -173,9 +162,8 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
               player.hp -= 5 + Math.round(Math.random() * 5);
               yourTurn = true;
             }, 500);
-          } 
+          }
         }
-
       }
 
       if (enemy.resistFear <= 0) {
@@ -209,10 +197,12 @@ export function battle(): Phaser.Types.Scenes.SettingsConfig | Phaser.Types.Scen
         this.scene.start(battleSceneKey);
       }
     },
-  }
+  };
 }
 
-function clearNotes(playedNotes: {s: Phaser.GameObjects.Image, hit: boolean}[]) {
+function clearNotes(
+  playedNotes: { s: Phaser.GameObjects.Image; hit: boolean }[]
+) {
   const count = playedNotes.filter((s) => s.hit).length;
   playedNotes.forEach((note) => note.s.destroy());
   while (playedNotes.length > 0) playedNotes.pop();
