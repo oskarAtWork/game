ebe90f61d4704e946f1bb2a6e569b40dfe7a63f8
MollyK-A <126957884+jessicajacobson@users.noreply.github.com>
2023-06-06 16:44:49 +0200

    attack times

--- parse-midi.py
+++ parse-midi.py
@@ -17,7 +17,29 @@ def get_note_timestamps(midi_file):
 
 
 # Usage example
-timestamps = get_note_timestamps('./assets/bird_attack_music/sh_attack.mid')
+sh_timestamps = get_note_timestamps('./assets/bird_attack_music/sh_attack.mid')
+ba_timestamps = get_note_timestamps('./assets/bird_attack_music/ba_attack.mid')
+tb_timestamps = get_note_timestamps('./assets/bird_attack_music/tb_attack.mid')
+ko_timestamps = get_note_timestamps('./assets/bird_attack_music/ko_attack.mid')
 
-for note, timestamp in timestamps:
-    print(f" [{note-42}, {int(timestamp)}], ")
+sh_attack_times = []
+ba_attack_times = []
+tb_attack_times = []
+ko_attack_times = []
+
+for note, timestamp in sh_timestamps:
+    # print(f" [{note-42}, {int(timestamp)}], ")
+    sh_attack_times.append([note-42, int(timestamp)])
+print(f"sh_attack_times = {sh_attack_times}")
+
+for note, timestamp in ba_timestamps:
+    ba_attack_times.append([note-42, int(timestamp)])
+print(f"ba_attack_times = {ba_attack_times}")
+
+for note, timestamp in ko_timestamps:
+    ko_attack_times.append([note-42, int(timestamp)])
+print(f"ko_attack_times = {ko_attack_times}")
+
+for note, timestamp in tb_timestamps:
+    tb_attack_times.append([note-42, int(timestamp)])
+print(f"tb_attack_times = {tb_attack_times}")
