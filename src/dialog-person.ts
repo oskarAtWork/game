4b50cefecb5b218b9cfc0f2d6c23ca09f03a522e
oskar olausson <oskar@oskars-MacBook-Pro.local>
2024-07-24 11:07:34 +0200

    svan

--- src/dialog-person.ts
+++ src/dialog-person.ts
@@ -1,85 +1,111 @@
-import adamUrl from '../assets/adam.png';
-import adamPlay1 from '../assets/adam-play-1.png';
-import adamPlay2 from '../assets/adam-play-2.png';
-import adamPlay3 from '../assets/adam-play-3.png';
-import adamPlay4 from '../assets/adam-play-4.png';
-import mollyUrl from '../assets/molly.png';
-import oskarUrl from '../assets/oskar.png';
-import biatareUrl from '../assets/biatare_sheet.png';
-import tajgaUrl from '../assets/tajgablastjart_sheet.png';
-import silkäsHägerUrl from '../assets/silkeshager_sheet.png';
-import klaraUrl from '../assets/klara.png';
-import spegelUrl from '../assets/spegel.png';
-import { EnterAction } from './dialogue_script/scene-utils';
-import { exhaust } from './helper';
-
+import adamUrl from "../assets/adam.png";
+import adamPlay1 from "../assets/adam-play-1.png";
+import adamPlay2 from "../assets/adam-play-2.png";
+import adamPlay3 from "../assets/adam-play-3.png";
+import adamPlay4 from "../assets/adam-play-4.png";
+import mollyUrl from "../assets/molly.png";
+import oskarUrl from "../assets/oskar.png";
+import svanUrl from "../assets/svan.png";
+import biatareUrl from "../assets/biatare_sheet.png";
+import tajgaUrl from "../assets/tajgablastjart_sheet.png";
+import silkäsHägerUrl from "../assets/silkeshager_sheet.png";
+import klaraUrl from "../assets/klara.png";
+import spegelUrl from "../assets/spegel.png";
+import { EnterAction } from "./dialogue_script/scene-utils";
+import { exhaust } from "./helper";
 
 export type DialogPerson = {
-  s: Phaser.GameObjects.Image; 
+  s: Phaser.GameObjects.Image;
   name: string;
   target_x: number;
   target_y: number;
-  x: number,
+  x: number;
   y: number;
-}
+};
 
 export function preloadPeople(scene: Phaser.Scene) {
-  scene.load.image('adam', adamUrl);
-  scene.load.image('molly', mollyUrl);
-  scene.load.image('oskar', oskarUrl);
-  scene.load.image('spegel', spegelUrl);
-  
-  scene.load.spritesheet('silkeshäger', silkäsHägerUrl, { frameWidth: 200, frameHeight: 200});
-  scene.load.spritesheet('tajga', tajgaUrl, { frameWidth: 200, frameHeight: 200});
-  scene.load.spritesheet('biatare', biatareUrl, { frameWidth: 200, frameHeight: 200});
-  scene.load.image('klara', klaraUrl);
+  scene.load.image("adam", adamUrl);
+  scene.load.image("molly", mollyUrl);
+  scene.load.image("oskar", oskarUrl);
+  scene.load.image("spegel", spegelUrl);
 
-  scene.load.image('adam-play-1', adamPlay1);
-  scene.load.image('adam-play-2', adamPlay2);
-  scene.load.image('adam-play-3', adamPlay3);
-  scene.load.image('adam-play-4', adamPlay4);
+  scene.load.spritesheet("silkeshäger", silkäsHägerUrl, {
+    frameWidth: 200,
+    frameHeight: 200,
+  });
+  scene.load.spritesheet("tajga", tajgaUrl, {
+    frameWidth: 200,
+    frameHeight: 200,
+  });
+  scene.load.spritesheet("biatare", biatareUrl, {
+    frameWidth: 200,
+    frameHeight: 200,
+  });
+  scene.load.image("svan", svanUrl);
+  scene.load.image("klara", klaraUrl);
+
+  scene.load.image("adam-play-1", adamPlay1);
+  scene.load.image("adam-play-2", adamPlay2);
+  scene.load.image("adam-play-3", adamPlay3);
+  scene.load.image("adam-play-4", adamPlay4);
 }
 
-export type BirdNames = 'silkeshäger' | 'biatare' | 'tajga';
-export type Names = 'oskar' | 'molly' | 'adam' | 'klara' | 'svan' | 'spegel' | BirdNames;
+export type BirdNames = "silkeshäger" | "biatare" | "tajga" | "svan";
+export type Names = "oskar" | "molly" | "adam" | "klara" | "spegel" | BirdNames;
 
 export const xPosition = (name: Names): number => {
   switch (name) {
-    case 'adam':
+    case "adam":
       return 150;
 
-    case 'molly':
+    case "molly":
       return 500;
-      
-    case 'oskar':
+
+    case "oskar":
       return 700;
 
-    case 'klara':
+    case "klara":
       return 600;
 
-    case 'spegel':
+    case "spegel":
       return 400;
 
-    case 'silkeshäger':
-    case 'biatare':
-    case 'tajga':
-    case 'svan':
+    case "silkeshäger":
+    case "biatare":
+    case "tajga":
       return 500;
-  
+
+    case "svan":
+      return 600;
+
     default:
       exhaust(name);
       return 0;
   }
-}
+};
 
 export const yPosition = () => 470;
 
-export function createPerson(scene: Phaser.Scene, name: Names, enterAction?: EnterAction): DialogPerson {
+export function createPerson(
+  scene: Phaser.Scene,
+  name: Names,
+  enterAction?: EnterAction
+): DialogPerson {
   const defX = xPosition(name);
   const defY = yPosition();
 
-  const x = enterAction?.from === 'left' ? -100 : enterAction?.from === 'right' ? 900 : defX;
-  const y = enterAction?.from === 'top' ? -100 : enterAction?.from === 'bottom' ? 900 : defY;
+  const x =
+    enterAction?.from === "left"
+      ? -100
+      : enterAction?.from === "right"
+      ? 900
+      : defX;
+  const y =
+    enterAction?.from === "top"
+      ? -100
+      : enterAction?.from === "bottom"
+      ? 900
+      : defY;
 
   return {
     name,
@@ -88,10 +114,15 @@ export function createPerson(scene: Phaser.Scene, name: Names, enterAction?: Ent
     y,
     target_x: x,
     target_y: y,
-  }
+  };
 }
 
-export function updatePerson(person: DialogPerson, talking: boolean, animationT: number, animation: [number, number][]) {
+export function updatePerson(
+  person: DialogPerson,
+  talking: boolean,
+  animationT: number,
+  animation: [number, number][]
+) {
   person.x = person.x * 0.95 + person.target_x * 0.05;
   person.y = person.y * 0.95 + person.target_y * 0.05;
   const [dx, dy] = animation[animationT % animation.length];
--- src/dialogue_script/scene5.ts
+++ src/dialogue_script/scene5.ts
@@ -1,12 +1,30 @@
 import { Line, adam, svan } from "./scene-utils";
 
- const scene: Line[] = [
-    adam('Känns ju lite skumt att mörda fåglar så här mycket'),
-    adam('Hade kännts bättre om jag kunde rädda en fågel också'),
-    // svan toner
-    adam('En svan som sitter fast! '),
-    svan('*pust* *puh* hjäääälp mig jag sitter fast *host*')
-    
+const scene: Line[] = [
+  adam("Känns ju lite skumt att mörda fåglar så här mycket"),
+  adam("Hade kännts bättre om jag kunde rädda en fågel också"),
+  // svan toner
+  adam("En svan som sitter fast! "),
+  svan("*pust* *puh* hjäääälp mig jag sitter fast *host*", {
+    otherAction: { type: "enter", from: "bottom" },
+  }),
+  adam("Täljkniv!!! Eller vänta, här är ju en chans att hjälpa..."),
+  adam("Tälkniv!!! fast hjälpsamt!"),
+  svan("Woah, tack, är du påväg upp för berget"),
+  adam("Japp, har nog en bit kvar"),
+  svan(
+    "Ja, det är typ 4 världar att besegra, i din väg står 9 faktioner, alla med 3 kungar att besegra"
+  ),
+  svan("...bergatroll, lava-flum-rides ..."),
+  svan("...drakar, 3 kolibrier i en rock som försöker komma in på bio..."),
+  svan("...demoner, jesus"),
+  adam("..."),
+  svan("men som gentjänst så kan jag ju bara flyga dig till toppen?"),
+  adam("toppen?"),
+  svan("toppen, då kör vi!!!", { otherAction: { type: "exit", to: "top" } }),
+  adam("[tänk att han flyger med på svanen]", {
+    otherAction: { type: "exit", to: "top" },
+  }),
 ];
 
 export default scene;
